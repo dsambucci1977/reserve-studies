1d0b29f6c3f3c32120d2b76c3bc284c438abbd5c
dsambucci1977 <support@pronoiasolutions.com>
2026-02-11 19:13:58 -0500

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -39,7 +39,7 @@ export default function Navigation() {
 
   return (
     <nav className="bg-white shadow-md border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           
           {/* Logo/Brand */}
